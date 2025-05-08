acd1b1c396b4c7d1c21c0fe2811903abbe47908e
Ebbjik <2772272916@qq.com>
2025-05-08 21:45:50 +0800

    添加快捷键
    jj退出插入模式，并切换到英文输入法
    进入插入模式时再切换到中文输入法

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,25 @@ vim.keymap.set("n", "<leader>p", '"+p', { desc = "从剪贴板粘贴" })
 vim.keymap.set("n", "<leader>P", '"+P', { desc = "从剪贴板粘贴到光标前" })
 vim.keymap.set("v", "<leader>p", '"+p', { desc = "粘贴到选中的区域" })
 vim.keymap.set("v", "<leader>P", '"+P', { desc = "粘贴到选中的区域" })
+
+-- 切换输入法函数 (fcitx4)
+local function switch_input_en()
+  vim.fn.jobstart("fcitx-remote -c")
+end
+
+local function switch_input_zh()
+  vim.fn.jobstart("fcitx-remote -o")
+end
+
+-- i模式 jj => 切英文 + 退出插入
+vim.keymap.set("i", "jj", function()
+  switch_input_en()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+end, { noremap = true, silent = true })
+
+-- 进入插入模式时自动切中文
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    switch_input_zh()
+  end,
+})
